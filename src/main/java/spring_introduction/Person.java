7cb3167e5f379a373488f5e3ba10a0cfd88e6051
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-06-03 22:00:45 +0300

    Annotation @Qualifier

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -1,27 +1,30 @@
 package spring_introduction;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("PersonBean")
 public class Person {
     @Autowired
+    @Qualifier("catBean")
     private Pet pet;
     private String surname;
     private int age;
 
-//    @Autowired
-//    public Person(Pet pet) {
-//        System.out.println("Person bean is created");
-//        this.pet = pet;
-//    }
-
-    public Person (){
+    @Autowired
+        public Person(@Qualifier("catBean") Pet pet) {
         System.out.println("Person bean is created");
+        this.pet = pet;
     }
 
+//    public Person (){
+//        System.out.println("Person bean is created");
+//    }
+
     // pet ->setPet
-    @Autowired
+//    @Autowired
+//    @Qualifier("dog")
     public void setPet(Pet pet) {
         System.out.println("Class Person: set pet");
         this.pet = pet;
