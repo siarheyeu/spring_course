7f152b1716904493195f977110d476b17639972b
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-06-20 23:55:18 +0300

    Advice @After

--- src/main/java/aop/aspects/UniversityLoggingAspect.java
+++ src/main/java/aop/aspects/UniversityLoggingAspect.java
@@ -1,10 +1,7 @@
 package aop.aspects;
 
 import aop.Student;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -35,11 +32,17 @@ public class UniversityLoggingAspect {
 //        System.out.println("afterReturningGetStudentsLoggingAdvice: логируем получение " +
 //                "списка студентов после работы метода getStudents");
 //    }
-    @AfterThrowing(pointcut = "execution(* getStudents())"
-        , throwing = "exception")
-    public void afterThrowingGetStudentsLoggingAdvice(Throwable exception){
-        System.out.println("afterThrowingGetStudentsLoggingAdvice: логируем выброс " +
-            "исключения" + exception);
+//    @AfterThrowing(pointcut = "execution(* getStudents())"
+//        , throwing = "exception")
+//    public void afterThrowingGetStudentsLoggingAdvice(Throwable exception){
+//        System.out.println("afterThrowingGetStudentsLoggingAdvice: логируем выброс " +
+//            "исключения" + exception);
+//    }
+
+    @After("execution(* getStudents())")
+    public void afterGetStudentsLoggingAdvice(){
+        System.out.println("afterGetStudentsLoggingAdvice: логируем нормальное " +
+                "окончание работы метода или выброс исключения");
     }
 
 }
